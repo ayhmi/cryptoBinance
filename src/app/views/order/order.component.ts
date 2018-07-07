0dba0e6c146ad5029e49314a238cbbf14ec97b0e
Ömer Eskizara <omereskizara@gmail.com>
2018-07-08 00:47:42 +0300

    price display success

--- src/app/views/order/order.component.ts
+++ src/app/views/order/order.component.ts
@@ -8,13 +8,16 @@ import { ExchangeInfo, SymbolInfo } from "../../model/binance";
 export class OrderComponent implements OnInit {
   private exchangeInfo: ExchangeInfo;
   private currentSymbol: string;
+  private price: number;
   constructor(private binanceService: BinanceService) {}
 
   ngOnInit() {
+    this.price = 0.0;
     this.binanceService.getExchangeInfo()
       .subscribe(exchangeInfo => {
         this.exchangeInfo = exchangeInfo;
         this.currentSymbol = exchangeInfo.symbols[0].symbol;
+        this.updatePrice();
       },
     err => {
         console.log(err);
@@ -24,6 +27,17 @@ export class OrderComponent implements OnInit {
   public setCurrentSymbol(symbolName)
   {
     this.currentSymbol = symbolName;
+    this.updatePrice();
   }
 
+  public updatePrice()
+  {
+    this.binanceService.getTicker(this.currentSymbol)
+      .subscribe(ticker => {
+        this.price = ticker.price;
+      },
+    err => {
+        console.log(err);
+    });
+  }
 }
