5667b998fd84579dc145649f9cb1525b359bd488
Ömer Eskizara <omereskizara@gmail.com>
2018-07-07 23:42:48 +0300

    additions for order window

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -12,7 +12,7 @@ import { RegisterComponent } from './views/register/register.component';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: 'order',
     pathMatch: 'full',
   },
   {
--- src/app/views/order/order.module.ts
+++ src/app/views/order/order.module.ts
@@ -4,11 +4,13 @@ import { OrderComponent } from './order.component';
 import { OrderRoutingModule } from './order-routing.module';
 
 import {CommonModule} from '@angular/common';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @NgModule({
   imports: [
     OrderRoutingModule,
-    CommonModule
+    CommonModule,
+    TabsModule,
   ],
   declarations: [ OrderComponent ]
 })
