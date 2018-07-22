9ebf768523a997bfa3849ceb37d63e41618c9b41
Ömer Eskizara <omereskizara@gmail.com>
2018-07-23 01:03:02 +0300

    warnings removed

--- src/app/views/order/amount-buttons/amount-buttons.component.ts
+++ src/app/views/order/amount-buttons/amount-buttons.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
 import { AccountInfo, OrderInfo } from "../../../model/binance";
 
 
@@ -19,12 +19,14 @@ export class AmountButtonsComponent implements OnInit {
   }
 
   setPercentageAmount(perctg, symbo) {
-    this.acc.balances.forEach(function(e){
-      if (`${e.asset}BTC` === symbo) {
-        let newAmount = parseFloat(e.free) * perctg
-        console.log(e)
-        this.amntChange.emit(newAmount)
-      }
-    }.bind(this);
+    this.acc.balances.forEach(
+      function(e) {
+        if (`${e.asset}BTC` === symbo) {
+          let newAmount = parseFloat(e.free) * perctg;
+          console.log(e);
+          this.amntChange.emit(newAmount);
+        }
+      }.bind(this)
+    )
   }
 }
